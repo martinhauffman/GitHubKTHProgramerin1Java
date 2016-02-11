cd7ead193a70fc8f6ab8d359447b9de855be059b
martinhauffman <martin.hauffman@gmail.com>
2016-02-11 12:52:26 +0100

    Fruff

---
+++ Exercises/src/Ch2/Ch2.java
@@ -0,0 +1,37 @@
+/**
+ * 
+ */
+package Ch2;
+/**
+ * @author martin.hauffman
+ *
+ */
+public class Ch2 {
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int enSiffra = 0;
+		int annanSiffra;
+		long tredjeSiffra;
+		float decimaltal1, decimaltal2;
+		double decimalDouble1, decimalDouble2;
+		
+
+		tredjeSiffra = 9999999999L;
+		decimaltal1 = 2.4556782345673456789F;
+		decimaltal2 = 123456789123456789.123456789F;
+		decimalDouble1 =  123456789123456789.123456789;
+		decimalDouble2 = (decimalDouble1 * 2);
+		
+		System.out.println("enSiffra = " + enSiffra);
+		System.out.println("tredjeSiffra = " + tredjeSiffra);
+		System.out.println("decimaltal1 = " + decimaltal1);
+		System.out.println("decimaltal2 = " + decimaltal2);
+		System.out.println("decimalDouble1 = " + decimalDouble1);
+		System.out.println("decimalDouble2 = " + decimalDouble2);
+	}
+
+}
